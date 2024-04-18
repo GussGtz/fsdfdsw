39d8c7bdd945c8f4c81c5e9c93ac9ad3d82fd049
Jonathan Hernandez <jonathanhernandez@Laptop-de-Jonathan.local>
2024-04-18 00:33:14 -0500

    Redirecciones de cards del HOME

--- src/partials/dashboard/DashboardCard01.jsx
+++ src/partials/dashboard/DashboardCard01.jsx
@@ -7,15 +7,19 @@ function DashboardCard01() {
   const imageUrl = 'https://www.pnguniverse.com/wp-content/uploads/2022/03/Gallina-corree-3456023f.png';
 
   return (
+    <>
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <div className="px-5 pt-5">
+      <Link to='/gallina'>
         <header className="flex justify-between items-start mb-2">
           <img src={Icon} width="32" height="32" alt="Icon 01" />
         </header>
         <h2 className="text-2xl font-bold text-slate-800 dark:text-slate-100 mb-2">Gallina</h2>
         {imageUrl && <img src={imageUrl} alt="Pollito" className="w-60 h-70 mb-2" />}
+      </Link>
       </div>
     </div>
+    </>
   );
 }
 
--- src/partials/dashboard/DashboardCard02.jsx
+++ src/partials/dashboard/DashboardCard02.jsx
@@ -8,11 +8,13 @@ function DashboardCard02() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <div className="px-5 pt-5">
+      <Link to='/pato'>
         <header className="flex justify-between items-start mb-2">
           <img src={Icon} width="32" height="32" alt="Icon 02" />          
         </header>
         <h2 className="text-2xl font-semibold text-slate-800 dark:text-slate-100 mb-2">Pato</h2>
         {imageUrl && <img src={imageUrl} alt="Pato" className="w-60 h-70 mb-2" />}
+      </Link>
       </div>
     </div>
   );
